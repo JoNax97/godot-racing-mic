d0a6d6104c2b1109531312cd17b2030755fcd7d4
LUCA <lucamastra08@gmail.com>
2023-08-25 15:32:04 -0300

    racing car

---
+++ PathFollow.gd
@@ -0,0 +1,40 @@
+extends PathFollow
+
+onready var path :Path = $".."
+var length
+var position = 0
+export(float) var speed = 1
+
+var effect : AudioEffectRecord
+var bus_index
+
+onready var audio_stream_player := $AudioStreamPlayer
+
+
+func _ready():
+	length = path.curve.get_baked_length()
+	
+	bus_index = AudioServer.get_bus_index("Microphone")
+	
+func _process(delta):
+	position += speed * delta 
+	
+	var pos_normalized = position/length
+	
+	unit_offset = pos_normalized
+	
+	var volume = db2linear(AudioServer.get_bus_peak_volume_left_db(bus_index, 0))
+	
+	position = volume * speed * delta
+	
+	
+
+
+
+
+
+
+
+
+
+
