afe1cb8553eb200ef6826e308a1aaabaf4323547
JoNax_97 <joaco.munix@gmail.com>
2023-08-25 16:18:05 -0300

    max speed

---
+++ SpeedArea.gd
@@ -0,0 +1,14 @@
+extends Area
+
+class_name SpeedArea
+
+export var max_speed: float
+
+func _on_body_entered(body):
+	if body.name == "player_car":
+		GlobalData.max_speed = max_speed
+
+
+func _on_body_exited(body):
+	if body.name == "player_car":
+		GlobalData.max_speed = GlobalData.global_max_speed
