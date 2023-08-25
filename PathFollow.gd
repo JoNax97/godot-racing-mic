c7f5b7ec685b38c2607782386cc3fbefa3755bdb
LUCA <lucamastra08@gmail.com>
2023-08-25 15:38:44 -0300

    ...

--- PathFollow.gd
+++ PathFollow.gd
@@ -4,12 +4,11 @@ onready var path :Path = $".."
 var length
 var position = 0
 export(float) var speed = 1
-
 var effect : AudioEffectRecord
 var bus_index
 
 onready var audio_stream_player := $AudioStreamPlayer
-
+onready var label_3d = $Camera/Label3D
 
 func _ready():
 	length = path.curve.get_baked_length()
@@ -17,7 +16,6 @@ func _ready():
 	bus_index = AudioServer.get_bus_index("Microphone")
 	
 func _process(delta):
-	position += speed * delta 
 	
 	var pos_normalized = position/length
 	
@@ -26,6 +24,8 @@ func _process(delta):
 	var volume = db2linear(AudioServer.get_bus_peak_volume_left_db(bus_index, 0))
 	
 	position = volume * speed * delta
+	
+	Label.text
 	
 	
 
